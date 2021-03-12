79dfa89e1e7335602c10d4e48293c1b91c27d375
ehdqoddl6 <hays39@naver.com>
2021-03-12 22:48:46 +0900

    20210312 backjoon study

--- backjoon/etc/9205.py
+++ backjoon/etc/9205.py
@@ -1,39 +1,45 @@
 
+from collections import deque
 n = int(input())
 
 
-def solution(loc):
-    
-    dis = []
-    for i in range(len(loc)-1):
-        sum = 0
-        for j in range(len(loc[i])):
+def solution(x, y):
 
-            sum += abs((loc[i][j] - loc[i+1][j]))
-        dis.append(sum)
+    queue = deque()
+    visited = []
 
-    for x in dis:
-        if(x > 1000):
-            return "sad"
+    queue.append([x,y])
+    visited.append([x,y])
+
+    while(queue):
+        x, y = queue.popleft()
+        if(x == x_end and y == y_end):
+            return 'happy'
+        
+        for nx, ny in loc:
+            if([nx, ny] not in visited):
+                distance = abs(nx-x) + abs(ny-y)
+                if(distance <= 1000):
+                    queue.append([nx, ny])
+                    visited.append([nx, ny]) 
     
-    return "happy"
+    return 'sad'
 
 result = []
 for i in range(n):
+
     loc = []
     con = int(input())
-    a, b = map(int, input().split())
-    loc.append([a, b])
-
-    for j in range(con):
-        a, b = map(int, input().split())
-
-        loc.append([a, b])
-
-    a, b = map(int, input().split())
-    loc.append([a, b])
+    x_start, y_start = map(int, input().split())
     
-    result.append(solution(loc))
+    for j in range(con):
+        x, y = map(int, input().split())
+        loc.append([x, y])
+
+    x_end, y_end = map(int, input().split())
+    loc.append([x_end, y_end])
+    
+    result.append(solution(x_start, y_start))
 
 
 for x in result:
