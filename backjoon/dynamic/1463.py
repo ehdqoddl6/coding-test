cb73f117ff78f206ab62445a855319060015174a
ehdqoddl6 <hays39@naver.com>
2021-04-01 14:34:18 +0900

    20210401 backjoon study

---
+++ backjoon/dynamic/1463.py
@@ -0,0 +1,19 @@
+n = int(input())
+
+def solution(n):
+
+    d = [0] * 1000001
+
+    for i in range(2, n+1):
+
+        d[i] = d[i-1] + 1
+
+        if(i%2 ==0):
+            d[i] = min(d[i], d[i//2] + 1)
+        
+        if(i%3 == 0):
+            d[i] = min(d[i], d[i//3] + 1)
+    
+    return d[n]
+
+print(solution(n))
