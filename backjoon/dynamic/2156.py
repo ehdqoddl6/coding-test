96b9cbc17a8a0d62957bed39e34b09200e0c3a03
ehdqoddl6 <hays39@naver.com>
2021-04-01 14:40:01 +0900

    20210401 backjoon study

--- backjoon/dynamic/2156.py
+++ backjoon/dynamic/2156.py
@@ -7,10 +7,12 @@ for i in range(n):
 
 d = [0]
 d.append(w[1])
-d.append(w[1]+w[2])
 
-for i in range(3, n + 1):
-    d.append(max(d[i - 1], d[i - 3] + w[i - 1] + w[i], d[i - 2] + w[i]))
+if(n>1):
+    d.append(w[1]+w[2])
+
+for i in range(3, n+1):
+    d.append(max(d[i-1], d[i-3] + w[i-1] + w[i], d[i-2] + w[i]))
 
 
 print(d[n])
